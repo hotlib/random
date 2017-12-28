2932a0c99bc5fe5edc0f30f79e48664e3a59ba65
Hotlib <jackiechanaaa@gmail.com>
2017-12-28 23:31:06 +0100

    changes due to GKE

--- dms/create-all.sh
+++ dms/create-all.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 kubectl create -f objectify-namespace.yaml
+kubectl create -f volume-claims.yaml
+sleep 5s
 kubectl create -f mysql-pod.yaml
 kubectl create -f solr-pod.yaml 
 kubectl create -f mysql-service.yaml 
 kubectl create -f solr-service.yaml 
 sleep 10s
-kubectl create -f jetty-deployment.yaml
+kubectl create -f jetty-pod.yaml
 kubectl create -f jetty-service.yaml 
--- dms/delete-all.sh
+++ dms/delete-all.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 kubectl delete --namespace objectify pod dms-mysql
 kubectl delete --namespace objectify pod dms-solr
-kubectl delete --namespace objectify deployment dms-jetty
+kubectl delete --namespace objectify pod dms-jetty
 kubectl delete --namespace objectify service mysql
 kubectl delete --namespace objectify service solr
 kubectl delete --namespace objectify service dms
+kubectl delete -f volume-claims.yaml
 kubectl delete namespace objectify 
 
