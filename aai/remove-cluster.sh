b19da38215738bc8904a6c50b770d606e501e1b5
Hotlib <jackiechanaaa@gmail.com>
2017-12-29 13:53:37 +0100

    change in volumes

--- aai/remove-cluster.sh
+++ aai/remove-cluster.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 kubectl delete --namespace aai-cassandra statefulset cassandra
 kubectl delete --namespace aai-cassandra service cassandra
-kubectl delete --namespace aai-cassandra pv cassandra-data
-
+#kubectl delete --namespace aai-cassandra pv cassandra-data
+kubectl delete namespace aai-cassandra
